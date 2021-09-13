fd6fcdf63bd175c44be23d4d913f5a09581e9800
Hendi Santika <hendisantika@gmail.com>
2021-09-14 06:38:35 +0700

    #5 - Add InvoiceLine Model

---
+++ src/main/java/com/hendisantika/model/InvoiceLine.java
@@ -0,0 +1,46 @@
+package com.hendisantika.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/09/21
+ * Time: 06.35
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "invoice_lines")
+public class InvoiceLine implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    /*
+     * The @JoinColumn is not necessary here, as by default
+     * JPA would create the column with the name "product_id"
+     */
+    //@JoinColumn(name="product_id")
+    private Product product;
+}
