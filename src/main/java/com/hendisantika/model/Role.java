dbd3c18a104748c53c8356032e8ead1f0fb8862d
Hendi Santika <hendisantika@gmail.com>
2021-09-14 06:40:16 +0700

    #6 - Add Role Model

---
+++ src/main/java/com/hendisantika/model/Role.java
@@ -0,0 +1,41 @@
+package com.hendisantika.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/09/21
+ * Time: 06.38
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "authorities", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "authority"})})
+//The uniqueContraints parameter allows you to set the unique key constraints that must be
+//set when creating the table. In this case, a unique constraint will be created
+//made up of the user_id and authority fields
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authority;
+}
