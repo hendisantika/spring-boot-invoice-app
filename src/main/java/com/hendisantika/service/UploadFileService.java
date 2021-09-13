daf0cb8e2be22c36b6c96f4f6f1c5f33962646fc
Hendi Santika <hendisantika@gmail.com>
2021-09-14 06:59:31 +0700

    #33 - Add load in UploadFileService

--- src/main/java/com/hendisantika/service/UploadFileService.java
+++ src/main/java/com/hendisantika/service/UploadFileService.java
@@ -2,7 +2,12 @@ package com.hendisantika.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,16 @@ public class UploadFileService {
     private final String UPLOADS_FOLDER = "uploads";
     private final Logger log = LoggerFactory.getLogger(getClass());
 
+    public Resource load(String filename) throws MalformedURLException {
+        //We load the path of the required image
+        Path photoPath = getPath(filename);
+        log.info("photoPath: " + photoPath);
+        Resource resource = null;
+        resource = new UrlResource(photoPath.toUri());
+        if (!resource.exists() || !resource.isReadable()) {
+            //If we try to load an image that does not exist we throw an exception
+            throw new RuntimeException("Error: Image cannot be loaded: " + photoPath);
+        }
+        return resource;
+    }
 }
