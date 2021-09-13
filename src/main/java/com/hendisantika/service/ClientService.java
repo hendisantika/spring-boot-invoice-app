8a260fc3316705639464da0ad6b8d699b6304a91
Hendi Santika <hendisantika@gmail.com>
2021-09-14 06:52:30 +0700

    #19 - Add ClientService

---
+++ src/main/java/com/hendisantika/service/ClientService.java
@@ -0,0 +1,29 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.ClientRepository;
+import com.hendisantika.repository.InvoiceRepository;
+import com.hendisantika.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/09/21
+ * Time: 06.51
+ */
+@Service
+public class ClientService {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+}
