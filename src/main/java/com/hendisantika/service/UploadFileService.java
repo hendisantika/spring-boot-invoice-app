65bcf063ecb9b87bcc571502964b18d29da66898
Hendi Santika <hendisantika@gmail.com>
2021-09-14 06:58:32 +0700

    #32 - Add UploadFileService

---
+++ src/main/java/com/hendisantika/service/UploadFileService.java
@@ -0,0 +1,21 @@
+package com.hendisantika.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/09/21
+ * Time: 06.58
+ */
+@Service
+public class UploadFileService {
+    private final String UPLOADS_FOLDER = "uploads";
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+}
