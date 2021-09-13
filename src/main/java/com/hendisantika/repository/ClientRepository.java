47c862b382e789206e0d3fc69e88a73f17cfcb7f
Hendi Santika <hendisantika@gmail.com>
2021-09-14 06:46:00 +0700

    #13 - Add save in ClientRepository

--- src/main/java/com/hendisantika/repository/ClientRepository.java
+++ src/main/java/com/hendisantika/repository/ClientRepository.java
@@ -28,4 +28,12 @@ public class ClientRepository {
     public List<Client> findAll() {
         return em.createQuery("from Client").getResultList();
     }
+
+    public void save(Client client) {
+        if (client.getId() != null && client.getId() > 0) {
+            em.merge(client);
+        } else {
+            em.persist(client);
+        }
+    }
 }
