15fc5a114987cab72161bfbd01bd7c38e8c8d4d0
Hendi Santika <hendisantika@gmail.com>
2021-09-14 06:44:03 +0700

    #9 - Add Client Model

---
+++ src/main/java/com/hendisantika/model/Client.java
@@ -0,0 +1,61 @@
+package com.hendisantika.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/09/21
+ * Time: 06.42
+ */
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String surname;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd-mm-yyyy")
+    @NotNull
+    private Date createdAt;
+
+    private String photo;
+
+    @OneToMany(mappedBy = "client", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Invoice> invoices;
+
+    public Client() {
+        invoices = new ArrayList<>();
+    }
+}
