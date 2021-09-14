8d9291aa7c3d6ef15309d8200fd6369df515c287
Hendi Santika <hendisantika@gmail.com>
2021-09-15 05:56:35 +0700

    #68 - Add create in InvoiceController

--- src/main/java/com/hendisantika/controller/InvoiceController.java
+++ src/main/java/com/hendisantika/controller/InvoiceController.java
@@ -1,11 +1,18 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Client;
+import com.hendisantika.model.Invoice;
 import com.hendisantika.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +31,20 @@ public class InvoiceController {
 
     @Autowired
     private ClientService clientService;
+
+    @GetMapping("/form/{clientId}")
+    public String create(@PathVariable(value = "clientId") Long clientId, Map<String, Object> model,
+                         RedirectAttributes flash) {
+        Client client = clientService.findOne(clientId);
+        if (client == null) {
+            flash.addFlashAttribute("error", "There is no such client");
+            return "redirect:/clients";
+        }
+        Invoice invoice = new Invoice();
+        invoice.setClient(client);
+        model.put("invoice", invoice);
+        model.put("title", "Create invoice");
+
+        return "/invoices/form";
+    }
 }
