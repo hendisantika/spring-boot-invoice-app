2c9dbcc9d88c47e0a6fa5fe28ce7f9d16d5d65c5
Hendi Santika <hendisantika@gmail.com>
2021-09-15 05:39:59 +0700

    #56 - Add configureGlobal in SpringSecurityConfig

--- src/main/java/com/hendisantika/config/SpringSecurityConfig.java
+++ src/main/java/com/hendisantika/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import com.hendisantika.auth.handler.LoginSuccessHandler;
 import com.hendisantika.service.JPAUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -57,5 +58,36 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().permitAll()
                 .and()
                 .exceptionHandling().accessDeniedPage("/error_403");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder build) throws Exception {
+        //This code allows to implement users "in memory"
+        //UserBuilder users = User.withDefaultPasswordEncoder(); //Spring Boot 1.5.10
+		/*PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder(); //Spring Boot 2
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);	//Spring Boot 2
+		build.inMemoryAuthentication()
+		.withUser(
+				users.username("admin")
+				.password("admin")
+				.roles("ADMIN", "USER"))
+		.withUser(
+				users.username("cristian")
+				.password("cristian")
+				.roles("USER"));*/
+
+        //This code allows to implement users with database through JDBC
+        //You have to indicate the queries to execute to login and obtain the user roles
+		/*build.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("SELECT USERNAME, PASSWORD, ENABLED FROM USERS WHERE USERNAME=?")
+		.authoritiesByUsernameQuery(
+				"SELECT U.USERNAME, A.AUTHORITY "
+						+ "FROM AUTHORITIES A INNER JOIN USERS U "
+						+ "ON (A.USER_ID = U.ID) "
+						+ "WHERE U.USERNAME=?");*/
+
+        //This code allows to implement users with database through JPA.
+        build.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 }
