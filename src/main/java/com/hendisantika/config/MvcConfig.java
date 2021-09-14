95769772eb60b700da39f1cb8befd72272f1bfd5
Hendi Santika <hendisantika@gmail.com>
2021-09-15 05:28:43 +0700

    #46 - Add MvcConfig

---
+++ src/main/java/com/hendisantika/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.hendisantika.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/09/21
+ * Time: 05.28
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+}
