2902f68280da3d7e200bd5b246781a79bd354adf
Hendi Santika <hendisantika@gmail.com>
2021-09-14 07:03:59 +0700

    #40 - Add loadUserByUsername in JPAUserDetailsService

--- src/main/java/com/hendisantika/service/JPAUserDetailsService.java
+++ src/main/java/com/hendisantika/service/JPAUserDetailsService.java
@@ -1,11 +1,21 @@
 package com.hendisantika.service;
 
+import com.hendisantika.model.Role;
+import com.hendisantika.model.User;
 import com.hendisantika.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +33,24 @@ public class JPAUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            log.error("The user " + username + " no exist!");
+            throw new UsernameNotFoundException("The user " + username + " no exist!");
+        }
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : user.getRoles()) {
+            log.info("User role: " + role.getAuthority());
+            authorities.add(new SimpleGrantedAuthority(role.getAuthority()));
+        }
+        if (authorities.isEmpty()) {
+            log.error("User role " + username + " does not have assigned roles!");
+            throw new UsernameNotFoundException("User role " + username + " does not have assigned roles!");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.getEnabled(), true, true, true, authorities);
+    }
 }
