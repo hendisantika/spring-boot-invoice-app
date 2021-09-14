6a85c765c40ee8026dfd3693d4907879a9455fc0
Hendi Santika <hendisantika@gmail.com>
2021-09-15 05:38:48 +0700

    #55 - Add configure in SpringSecurityConfig

--- src/main/java/com/hendisantika/config/SpringSecurityConfig.java
+++ src/main/java/com/hendisantika/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import com.hendisantika.service.JPAUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -34,4 +35,27 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JPAUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Here we specify the roles required to access
+        //each route. The "long" way is to specify each path access here,
+        //but it can also be done directly in the controller, using
+        //the @Secured (role) annotation on each method
+        http.authorizeRequests()
+                .antMatchers("/", "/css/**", "/js/**", "/img/**", "/clients", "/locale").permitAll()
+                //.antMatchers("/ver/**").hasAnyRole("USER")
+                //.antMatchers("/uploads/**").hasAnyRole("USER")
+                //.antMatchers("/form/**").hasAnyRole("ADMIN")
+                //.antMatchers("/remove/**").hasAnyRole("ADMIN")
+                //.antMatchers("/invoice/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().successHandler(successHandler)
+                .loginPage("/login").permitAll()    //The .loginPage (path) method serves
+                .and()                                            //to tell Spring which path to use
+                .logout().permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/error_403");
+    }
 }
