e707cf86b8b14a21e1a93d2ced62b2368d2a2cb6
Hendi Santika <hendisantika@gmail.com>
2021-09-15 05:40:41 +0700

    #57 - Add ThymeleafConfig

---
+++ src/main/java/com/hendisantika/config/ThymeleafConfig.java
@@ -0,0 +1,37 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/09/21
+ * Time: 05.40
+ */
+@Configuration
+public class ThymeleafConfig {
+    @Bean
+    public SpringTemplateEngine springTemplateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(htmlTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver htmlTemplateResolver() {
+        SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
+        emailTemplateResolver.setPrefix("classpath:/templates");
+        emailTemplateResolver.setSuffix(".html");
+        // emailTemplateResolver.setTemplateMode(StandardTemplateModeHandlers.HTML5.getTemplateModeName());
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        return emailTemplateResolver;
+    }
+}
