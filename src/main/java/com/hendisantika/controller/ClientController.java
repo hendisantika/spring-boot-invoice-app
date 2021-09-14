3a19e9b51a3603571eae6f790084699f0a44f376
Hendi Santika <hendisantika@gmail.com>
2021-09-15 05:43:06 +0700

    #59 - Add verFoto in ClientController

--- src/main/java/com/hendisantika/controller/ClientController.java
+++ src/main/java/com/hendisantika/controller/ClientController.java
@@ -6,8 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.net.MalformedURLException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,5 +43,26 @@ public class ClientController {
 
     @Autowired
     private MessageSource messageSource;
+
+    //Method for uploading images
+    //We write the parameter {filename:. +} As a regular expression
+    //since if not Spring would remove the extension of it (from the file)
+    //and we need it to find the file on the computer.
+    //When the view tries to upload the image from the uploads folder
+    //this method is called
+    @Secured("ROLE_USER")
+    @GetMapping(value = "/uploads/{filename:.+}")
+    public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
+        Resource resource = null;
+        try {
+            resource = uploadFileService.load(filename);
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment: filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
 
 }
