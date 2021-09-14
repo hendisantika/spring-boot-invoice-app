5a2df05f328ca379bec218aa890228f323b094af
Hendi Santika <hendisantika@gmail.com>
2021-09-15 05:35:56 +0700

    #53 - Add LoginSuccessHandler

---
+++ src/main/java/com/hendisantika/auth/handler/LoginSuccessHandler.java
@@ -0,0 +1,41 @@
+package com.hendisantika.auth.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.support.SessionFlashMapManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-invoice-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/09/21
+ * Time: 05.34
+ */
+@Component
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
+        FlashMap flashMap = new FlashMap();
+
+        flashMap.put("success", "Welcome " + authentication.getName() + ", you have successfully logged in");
+        flashMapManager.saveOutputFlashMap(flashMap, request, response);
+        if (authentication != null) {
+            logger.info("The user " + authentication.getName() + " you have logged in successfully.");
+        }
+
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
